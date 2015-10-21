1ae74ecc822e3ab37a0efd5ce0f0ed0e9ca95d9b
c.hansen <c.hansen@141.19.145.72>
2015-10-21 10:30:17 +0200

    Added class VM with variables

---
+++ src/VM.java
@@ -0,0 +1,40 @@
+
+public class VM {
+
+	final int NOP = 0;
+	final int LOAD = 1;
+	final int MOV = 2;
+	final int ADD = 3;
+	final int SUP = 4;
+	final int MUL = 5;
+	final int DIV = 6;
+	final int PUSH = 7;
+	final int POP = 8;
+	final int JMP = 9;
+	final int JIZ = 10;
+	final int JIH = 11;
+	final int JSR = 12;
+	final int RTS = 13;
+	
+	int[] memory= new int[4096];
+	int[] register = new int[16];
+	
+	int PC = 0;
+			
+			
+	
+
+	void startVM() {
+
+		do {
+			
+			
+			
+			
+			
+
+		} while (true);
+
+	}
+
+}
